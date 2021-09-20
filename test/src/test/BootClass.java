3acfb3ed28ef0309fad27b67b0a859ef0677ba02
swetanarra <swnarra@LIN65004120.corp.capgemini.com>
2021-09-20 11:12:52 +0530

    boot class added

---
+++ test/src/test/BootClass.java
@@ -0,0 +1,17 @@
+package test;
+
+public class BootClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("HELLO");
+    show();
+	}
+	public static void show() {
+		int [] arr={12,34,56,3,4,5,6};
+    for(int i=0;i<arr.length;i++) {
+    	System.out.println(arr[i]);
+    }
+    System.out.println("showing show method");
+	}
+}
