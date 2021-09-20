4608d50defba73e55b87b5a891c037682e606310
swetanarra <swnarra@LIN65004120.corp.capgemini.com>
2021-09-20 11:24:36 +0530

    new workspace boot added

--- test/src/test/BootClass.java
+++ test/src/test/BootClass.java
@@ -9,8 +9,10 @@ public class BootClass {
 	}
 	public static void show() {
 		int [] arr={12,34,56,3,4,5,6};
-    for(int i=0;i<arr.length;i++) {
-    	System.out.println(arr[i]);
+		int sum=0;
+    for(int n:arr) {
+    	sum+=n;
+    	System.out.println(n);
     }
     System.out.println("showing show method");
 	}
